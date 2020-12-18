bd8dd5fb3aeb38774b0f690fbdbec2cb2ef391a3
DominickDejesus <34385118+DominickDejsus@users.noreply.github.com>
2020-12-18 14:25:02 -0500

    added gist read and create routes

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = require("express")();
 
 app.use(cors());
-
+const { getAllGists, createGist, getGist } = require("./api/gists");
 const { getAllTodos, postOneTodo } = require("./api/todos");
 const { loginUser, signUpUser, resetPassword } = require("./api/users");
 const { checkUser } = require("./util/security");
@@ -17,4 +17,8 @@ app.post("/login", loginUser);
 app.post("/signup", signUpUser);
 app.post("/password/reset", resetPassword);
 
+//gists
+app.post("/gists", createGist);
+app.get("/gists", getAllGists);
+app.get("/gists/:id", getGist);
 exports.api = functions.https.onRequest(app);
