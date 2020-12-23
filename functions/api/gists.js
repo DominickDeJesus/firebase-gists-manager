41dc2b2ffd4b06237be0155c9ed01ccb00310d33
DominickDeJesus <dominick.dejesus@gmail.com>
2020-12-22 22:03:48 -0500

    fixed broken deploy

--- functions/api/gists.js
+++ functions/api/gists.js
@@ -2,38 +2,37 @@ const { Octokit } = require("@octokit/rest");
 const GITHUB_TOKEN = require("firebase-functions").config().github.token;
 
 const octokit = new Octokit({
-  auth: GITHUB_TOKEN,
-  userAgent: "firebase-gist 1.0.0",
+	auth: GITHUB_TOKEN,
+	userAgent: "firebase-gist 1.0.0",
 });
 
 exports.getAllGists = async (req, res) => {
-  try {
-    const gists = await octokit.gists.listForUser({
-      username: "dominickdejesus",
-    });
-    res.json(gists);
-  } catch (error) {
-    console.log(error.message);
-  }
+	try {
+		const gists = await octokit.gists.listForUser({
+			username: "dominickdejesus",
+		});
+		res.json(gists);
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 
 exports.getGist = async (req, res) => {
-  try {
-    const gist = await octokit.gists.get({
-      gist_id: req.params.id,
-    });
-    res.json(gist);
-  } catch (error) {
-    console.log(error.message);
-  }
+	try {
+		const gist = await octokit.gists.get({
+			gist_id: req.params.id,
+		});
+		res.json(gist);
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 
 exports.createGist = async (req, res) => {
-  try {
-    const { desc, filename, fileContent } = req.body;
-    const gist = await octokit.gists.create(req.body);
-    res.json(gist);
-  } catch (error) {
-    console.log(error.message);
-  }
+	try {
+		const gist = await octokit.gists.create(req.body);
+		res.json(gist);
+	} catch (error) {
+		console.log(error.message);
+	}
 };
